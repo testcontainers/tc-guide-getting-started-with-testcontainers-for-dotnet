87bbf7e7fcb8a7c82452546e83ce6998cf6a9530
Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>
2023-05-02 09:36:01 +0200

    chore: Add .NET sources

---
+++ src/GettingStarted/Customer.cs
@@ -0,0 +1,3 @@
+﻿namespace GettingStarted;
+
+public readonly record struct Customer(long Id, string Name);
---
+++ src/GettingStarted/CustomerService.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+
+namespace GettingStarted;
+
+public sealed class CustomerService
+{
+    private readonly DbConnectionProvider _dbConnectionProvider;
+
+    public CustomerService(DbConnectionProvider dbConnectionProvider)
+    {
+        _dbConnectionProvider = dbConnectionProvider;
+        CreateCustomersTable();
+    }
+
+    public IEnumerable<Customer> GetCustomers()
+    {
+        IList<Customer> customers = new List<Customer>();
+
+        using var connection = _dbConnectionProvider.GetConnection();
+        using var command = connection.CreateCommand();
+
+        command.CommandText = "SELECT id, name FROM customers";
+        command.Connection.Open();
+
+        using var dataReader = command.ExecuteReader();
+        while (dataReader.Read())
+        {
+            var id = dataReader.GetInt64(0);
+            var name = dataReader.GetString(1);
+            customers.Add(new Customer(id, name));
+        }
+
+        return customers;
+    }
+
+    public void Create(Customer customer)
+    {
+        using var connection = _dbConnectionProvider.GetConnection();
+        using var command = connection.CreateCommand();
+
+        var id = command.CreateParameter();
+        id.ParameterName = "@id";
+        id.Value = customer.Id;
+
+        var name = command.CreateParameter();
+        name.ParameterName = "@name";
+        name.Value = customer.Name;
+
+        command.CommandText = "INSERT INTO customers (id, name) VALUES(@id, @name)";
+        command.Parameters.Add(id);
+        command.Parameters.Add(name);
+        command.Connection.Open();
+        command.ExecuteNonQuery();
+    }
+
+    private void CreateCustomersTable()
+    {
+        using var connection = _dbConnectionProvider.GetConnection();
+        using var command = connection.CreateCommand();
+        command.CommandText = "CREATE TABLE IF NOT EXISTS customers (id BIGINT NOT NULL, name VARCHAR NOT NULL, PRIMARY KEY (id))";
+        command.Connection.Open();
+        command.ExecuteNonQuery();
+    }
+}
